d81dfecc9fd6d1d12904d89f85833acd258cfe68
HugoTrwl <82870172+HugoTrwl@users.noreply.github.com>
2022-03-18 10:30:04 +0100

    light start C1 / light state P1

--- socket/server.js
+++ socket/server.js
@@ -1,7 +1,7 @@
 var SerialPort = require('serialport');
 var xbee_api = require('xbee-api');
 var C = xbee_api.constants;
-//var storage = require("./storage")
+var storage = require("./storage")
 require('dotenv').config()
 
 
@@ -23,23 +23,133 @@ serialport.pipe(xbeeAPI.parser);
 xbeeAPI.builder.pipe(serialport);
 
 serialport.on("open", function () {
-  var frame_obj = { // AT Request to be sent
+  /*frame_obj = { // AT Request to be sent
     type: C.FRAME_TYPE.AT_COMMAND,
-    command: "NI",
+    command: "SL",
     commandParameter: [],
   };
+  xbeeAPI.builder.write(frame_obj);*/
 
-  xbeeAPI.builder.write(frame_obj);
+  var frame_obj;
 
-  frame_obj = { // AT Request to be sent
+  //sleepmode OFF -> not working
+  frame_obj = {
     type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
     destination64: "FFFFFFFFFFFFFFFF",
-    command: "NI",
-    commandParameter: [],
+    command: "D8",
+    commandParameter: [4],
+  };
+  xbeeAPI.builder.write(frame_obj);
+  
+  frame_obj = {
+    type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
+    destination16: "FFFFFFFFFFFFFFFF",
+    command: "D2",
+    commandParameter: [03],
   };
   xbeeAPI.builder.write(frame_obj);
 
+  frame_obj = {
+    type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
+    destination16: "FFFFFFFFFFFFFFFF",
+    command: "D3",
+    commandParameter: [03],
+  };
+  xbeeAPI.builder.write(frame_obj);
+
+  frame_obj = {
+    type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
+    destination16: "FFFFFFFFFFFFFFFF",
+    command: "IC",
+    commandParameter: [C],
+  };
+  xbeeAPI.builder.write(frame_obj);
+
+  // éteint la lumière de tous les routeurs/proies
+  frame_obj = {
+    type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
+    destination64: "FFFFFFFFFFFFFFFF",
+    command: "D0",
+    commandParameter: [00],
+  };
+  xbeeAPI.builder.write(frame_obj);
+
+  // éteint la lumière du coordinateur
+  frame_obj = {
+    type: C.FRAME_TYPE.AT_COMMAND,
+    command: "D1",
+    commandParameter: [04],
+  };
+  xbeeAPI.builder.write(frame_obj);
+
+  //lightOnP1();
+  //lightOffP1();
+
+  /*frame_obj = { // AT Request to be sent
+    type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
+    destination64: "FFFFFFFFFFFFFFFF",
+    command: "SH",
+    commandParameter: [],
+  };
+  xbeeAPI.builder.write(frame_obj);*/
+
 });
+
+lightCoordinatorOn = function (address) {
+
+  frame_obj = {
+    type: C.FRAME_TYPE.AT_COMMAND,
+    command: "D1",
+    commandParameter: [05],
+  };
+  xbeeAPI.builder.write(frame_obj);
+
+}
+
+lightOff = function (address) {
+
+  frame_obj = {
+    type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
+    destination16: address,
+    command: "D0",
+    commandParameter: [00],
+  };
+  xbeeAPI.builder.write(frame_obj);
+
+}
+
+lightOn = function (address) {
+
+  frame_obj = {
+    type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
+    destination16: address,
+    command: "D0",
+    commandParameter: [05],
+  };
+  xbeeAPI.builder.write(frame_obj);
+
+}
+
+sleepmodeP1 = function () {
+
+  frame_obj = {
+    type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
+    destination16: "b7d1",
+    command: "SM",
+    commandParameter: [5],
+  };
+  xbeeAPI.builder.write(frame_obj);
+}
+
+dbCall = function () {
+  frame_obj = {
+    type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
+    destination16: "FFFFFFFFFFFFFFFF",
+    command: "DB",
+    commandParameter: [],
+  };
+  xbeeAPI.builder.write(frame_obj);
+}, 600;
 
 // All frames parsed by the XBee will be emitted here
 
@@ -61,16 +171,61 @@ xbeeAPI.parser.on("data", function (frame) {
   if (C.FRAME_TYPE.NODE_IDENTIFICATION === frame.type) {
     // let dataReceived = String.fromCharCode.apply(null, frame.nodeIdentifier);
     console.log("NODE_IDENTIFICATION");
-    //storage.registerSensor(frame.remote64)
+    console.log(frame.nodeIdentifier);
+    storage.registerPlayer(frame.remote64, frame.nodeIdentifier);
 
   } else if (C.FRAME_TYPE.ZIGBEE_IO_DATA_SAMPLE_RX === frame.type) {
 
-    console.log("ZIGBEE_IO_DATA_SAMPLE_RX")
-    console.log(frame.analogSamples.AD0)
-    //storage.registerSample(frame.remote64,frame.analogSamples.AD0 )
+    console.log("ZIGBEE_IO_DATA_SAMPLE_RX");
+    console.log(frame);
+    storage.registerSample(frame.remote64, 'toto');
 
+    if (frame.digitalSamples.DIO2 === 1) {
+      //déclarer le sleepmode
+      //sleepmodeP1();
+      console.log(frame, "lumière éteinte")
+      lightOff(frame.remote16);
+      storage.updateButton(frame.remote64, Date.now());
+      storage.updateStatePlayer(frame.remote64, false);
+    } else {
+      //storage.retrieveLastPressed(frame.remote64).then((player)=>console.log(player.data().lastPressed));
+      let lastPressedOld;
+      storage.retrieveLastPressed(frame.remote64).then((player) => {
+        lastPressedOld = player.data().lastPressed;
+
+        diff = Date.now() - lastPressedOld;
+        if (diff < 5000 && diff > 2000) {
+          console.log("entre 2 et 5 sec")
+          console.log("name : " + frame)
+          lightOn(frame.nodeIdentifier);
+          storage.updateStatePlayer(frame.remote64, true);
+        } else if (diff < 2000){
+          console.log("just clicked")
+        } else {
+          console.log("click error/lumière éteinte")
+        }
+        console.log(lastPressedOld,diff);
+      });
+    }
+
+    if (frame.digitalSamples.DIO3 === 1) {
+      console.log("lumière coordinateur allumée/partie lancée")
+      lightCoordinatorOn();
+      storage.updateButton(frame.remote64, Date.now());
+    }
+
+  } else if (C.FRAME_TYPE.ZIGBEE_EXPLICIT_RX === frame.type) {
+    console.log(frame)
+    if (frame.digitalSamples.DIO3 === 1) {
+      //lance la partie
+      console.log("partie lancée")
+      storage.startGame(frame.remote64, Date.now());
+      lightCoordinatorOn();
+    }
   } else if (C.FRAME_TYPE.REMOTE_COMMAND_RESPONSE === frame.type) {
     console.log("REMOTE_COMMAND_RESPONSE")
+    /*let buf = frame.commandData;
+    var int = buf.readUIntBE(0, Buffer.byteLength(buf));*/
   } else {
     console.debug(frame);
     let dataReceived = String.fromCharCode.apply(null, frame.commandData)
--- socket/storage.js
+++ socket/storage.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = require('./cache-cache-connecte-0d1a05d60761.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -8,13 +8,14 @@ admin.initializeApp({
 const db = admin.firestore();
 
 
-module.exports.registerSensor = async function (address) {
+module.exports.registerPlayer = async function (address, name) {
 
-  const docRef = db.collection('sensors').doc(address);
+  const docRef = db.collection('players').doc(address);
 
   const sensor = {
     address: address,
     date: Date.now(),
+    name: name
   }
 
   await docRef.get().then((snapshotDoc)=> {
@@ -23,6 +24,84 @@ module.exports.registerSensor = async function (address) {
     else
       docRef.update(sensor);
   })
+}
+
+/*module.exports.registerTypePlayer = async function (address, sample) {
+
+  const docRef = db.collection('sensors').doc(address)
+    .collection('samples').doc(Date.now().toString());
+
+  const data = {
+    value: sample,
+    date: Date.now(),
+  }
+  await docRef.set(data);
+
+
+}*/
+
+module.exports.updateButton = async function (address, date) {
+
+  const docRef = db.collection('players').doc(address);
+
+  const sensor = {
+    lastPressed: date,
+  }
+
+  await docRef.get().then((snapshotDoc)=> {
+    if (!snapshotDoc.exists)
+      docRef.set(sensor);
+    else
+      docRef.update(sensor);
+  })
+}
+
+module.exports.updateStatePlayer = async function (address, alive) {
+
+  const docRef = db.collection('players').doc(address);
+
+  const sensor = {
+    isAlive: alive,
+  }
+
+  await docRef.get().then((snapshotDoc)=> {
+    if (!snapshotDoc.exists)
+      docRef.set(sensor);
+    else
+      docRef.update(sensor);
+  })
+}
+
+module.exports.startGame = async function (address, date) {
+
+  const docRef = db.collection('hunter').doc(address);
+
+  const sensor = {
+    dateStartGame: date,
+  }
+
+  await docRef.get().then((snapshotDoc)=> {
+    if (!snapshotDoc.exists)
+      docRef.set(sensor);
+    else
+      docRef.update(sensor);
+  })
+}
+
+
+module.exports.retrievePlayer = function (address) {
+
+  const docRef = db.collection('players');
+
+  return docRef.get(address);
+
+}
+
+module.exports.retrieveLastPressed = function (address) {
+
+  const docRef = db.collection('players').doc(address);
+
+  return docRef.get();
 }
 
 module.exports.registerSample = async function (address, sample) {
